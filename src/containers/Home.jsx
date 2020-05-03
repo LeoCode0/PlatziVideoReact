dd2f638d301b6c8b183a713fc43085dc04d05670
Leonardo Espinoza <leoespinozidk@gmail.com>
2020-05-03 15:03:09 -0500

    Agregando el archivo de rutas con BrowserRouter

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -11,7 +11,7 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initialState';
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
 
   return initialState.length === 0 ? <h1>Loading...</h1> : (
@@ -49,4 +49,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
