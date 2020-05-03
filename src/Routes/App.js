65e01fd382d321437695582a60d2af7ebdd676f2
Leonardo Espinoza <leoespinozidk@gmail.com>
2020-05-03 16:11:17 -0500

    Moviendo de components a containers

--- src/Routes/App.js
+++ src/Routes/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
-import Login from '../components/Login';
-import Register from '../components/Register'
+import Login from '../containers/Login';
+import Register from '../containers/Register'
 
 const App = () => (
   <BrowserRouter>
