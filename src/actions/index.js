8cc4b4a4377d00ac98e11d56aea8aba11c494fb4
Leonardo Espinoza <leoespinozidk@gmail.com>
2020-05-16 22:29:01 -0500

    Creando register con redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,3 +22,8 @@ export const logoutRequest = (payload) => ({
   type: 'LOGOUT_REQUEST',
   payload,
 });
+
+export const registerRequest = (payload) => ({
+  type: 'REGISTER_REQUEST',
+  payload,
+});
--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,20 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/Login.scss';
 
-const Register = () => (
-  <section className='login'>
-    <section className='login__container'>
-      <h2>Regístrate</h2>
-      <form action='post' className='login__container--form'>
-        <input className='login__input' type='text' placeholder='Nombre' id='' />
-        <input className='login__input' type='text' placeholder='Correo' />
-        <input className='login__input' type='password' placeholder='Contraseña' />
-        <button className='button'>Registrame</button>
-      </form>
-      <p className='login__container--sesion'><Link to='/login'>Iniciar sesión</Link></p>
-    </section>
-  </section>
-);
+import { registerRequest} from '../actions/index';
 
-export default Register;
+const Register = (props) => {
+  const [form, setValues] = useState({
+    email: '',
+    name: '',
+    password: '',
+  });
+
+  const handleInput = (e) => {
+    setValues({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.registerRequest(form);
+    props.history.push('/');
+  };
+
+  return (
+    <section className='login'>
+      <section className='login__container'>
+        <h2>Regístrate</h2>
+        <form action='post' className='login__container--form' onSubmit={handleSubmit}>
+          <input
+            name='name'
+            className='login__input'
+            type='text'
+            placeholder='Nombre'
+            onChange={handleInput}
+            id=''
+          />
+          <input
+            name='email'
+            className='login__input'
+            type='text'
+            onChange={handleInput}
+            placeholder='Correo'
+          />
+          <input
+            name='password'
+            className='login__input'
+            type='password'
+            onChange={handleInput}
+            placeholder='Contraseña'
+          />
+          <button className='button'>Registrame</button>
+        </form>
+        <p className='login__container--sesion'><Link to='/login'>Iniciar sesión</Link></p>
+      </section>
+    </section>
+  );
+};
+
+const mapDispatchToProps = {
+  registerRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Register);
