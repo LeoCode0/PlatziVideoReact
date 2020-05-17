28406e6ddcf860a6e209b87a23cdc81c42a7f3a7
Leonardo Espinoza <leoespinozidk@gmail.com>
2020-05-16 21:57:35 -0500

    Usando gravatar para las fotos de perfil

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/Header.scss';
 import profile from '../assets/static/user.png';
 import logo from '../assets/static/platzi-video.png';
 
-const Header = () => (
-  <header className='header'>
-    <Link className='header' to='/'>
-      <img className='header__img' src={logo} alt='logo de platzi video' />
-    </Link>
-    <div className='header__menu'>
-      <div className='header__menu--profile'>
-        <img src={profile} alt='user' />
-        <p>Perfil</p>
-      </div>
-      <ul>
-        <li><Link to='/'>Cuenta</Link></li>
-        <li><Link to='/login'>Iniciar sesion</Link></li>
-      </ul>
-    </div>
-  </header>
-);
+import gravatar from '../utils/gravatar';
 
-export default Header;
+const Header = (props) => {
+  const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
+  return (
+    <header className='header'>
+      <Link className='header' to='/'>
+        <img className='header__img' src={logo} alt='logo de platzi video' />
+      </Link>
+      <div className='header__menu'>
+        <div className='header__menu--profile'>
+          {hasUser ?
+            <img src={gravatar(user.email)} alt={user.email} /> :
+            <img src={profile} alt='user' />}
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li><Link to='/'>Cuenta</Link></li>
+          <li><Link to='/login'>Iniciar sesion</Link></li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+export default connect(mapStateToProps, null)(Header);
